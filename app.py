f513d5cde994d80410bec0ecdb6ca53e54aa5b55
Aqilarik Nugra Rezkanintio <aqilarik.rezkanintio@tokopedia.com>
2022-06-07 21:05:23 +0700

    add key on json response

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def predict():
 
     prediction, confidence = process(model, img)
 
-    return jsonify(prediction, confidence)
+    return jsonify(prediction = prediction, confidence = confidence)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
